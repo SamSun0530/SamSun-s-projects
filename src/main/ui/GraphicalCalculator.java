b68ce46401415b4c733782a82b515a83a0e235de
AcorssHorizon <2298998611@qq.com>
2021-11-14 19:49:58 -0800

    phase3 calculator: stuck in calculation :(

--- src/main/ui/GraphicalCalculator.java
+++ src/main/ui/GraphicalCalculator.java
@@ -5,65 +5,117 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 // Main class create a CalculatorAPP
 public class GraphicalCalculator extends JFrame implements ActionListener {
     private JTextField field;
-
-    public GraphicalCalculator() {
-//        setButtons();
-//        pack();
-    }
-
-
-//    private void setButton(String command) {
-//        JButton button = new JButton(command);
-//        button.setActionCommand(command);
-//        button.addActionListener(this);
-//        button.setPreferredSize(new Dimension(120, 40));
-//        JLabel label = new JLabel();
-//        add(button);
-//        add(label);
-//    }
-//
-//    private void setButtons() {
-//        setButton("7");
-//        setButton("8");
-//        setButton("9");
-//        setButton("4");
-//        setButton("5");
-//        setButton("6");
-//        setButton("1");
-//        setButton("2");
-//        setButton("3");
-//        setButton("delete");
-//        setButton("0");
-//        setButton("confirm");
-//        setButton("+");
-//        setButton("-");
-//        setButton("*");
-//        setButton("/");
-//        setButton("^");
-//        setButton("clear");
-//    }
+    private String operator = null;
+    private String operand1;
+    private String operand2;
+    private int result;
+    private String[] operands;
+    private String text;
 
     public static void main(String[] args) {
         new GraphicalCalculator();
-        new CalculatorDisplay();
+    }
+
+    public GraphicalCalculator() {
+        setTitle("2 Integer inputs calculator");
+        setBackground();
+        setScreen();
+        setButtons();
+        pack();
+    }
+
+    // EFFECTS: set the background of calculator
+    private void setBackground() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setPreferredSize(new Dimension(500, 750));
+        setVisible(true);
+        setResizable(false);
+    }
+
+    // EFFECTS: set the screen of calculator
+    private void setScreen() {
+        field = new JTextField(40);
+        field.setPreferredSize(new Dimension(480, 100));
+        field.setEditable(false);
+        add(field);
+    }
+
+    private void setButton(String command) {
+        JButton button = new JButton(command);
+        button.setActionCommand(command);
+        button.addActionListener(this);
+        button.setPreferredSize(new Dimension(120, 40));
+        JLabel label = new JLabel();
+        add(button);
+        add(label);
+    }
+
+    private void setButtons() {
+        setButton("7");
+        setButton("8");
+        setButton("9");
+        setButton("4");
+        setButton("5");
+        setButton("6");
+        setButton("1");
+        setButton("2");
+        setButton("3");
+        setButton("delete");
+        setButton("0");
+        setButton("=");
+        setButton("+");
+        setButton("-");
+        setButton("*");
+        setButton("/");
+        setButton("^");
+        setButton("clear");
+        setButton("logs");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String text = field.getText() + e.getActionCommand();
-        field.setText(text);
-        Font font = new Font("SansSerif", Font.BOLD, 50);
-        field.setFont(font);
+        calculation(e);
+        displayOnScreen(e);
+    }
 
+    public void displayOnScreen(ActionEvent e) {
+        if (e.getActionCommand().equals("delete")) {
+            text = field.getText().substring(0, field.getText().length() - 1);
+            displayToScreen(text);
+        } else if (!e.getActionCommand().equals("clear")
+                && !e.getActionCommand().equals("logs")) {
+            text = field.getText() + e.getActionCommand();
+            displayToScreen(text);
+            System.out.println(text);
+        }
+    }
+
+    private void calculation(ActionEvent e) {
         if (e.getActionCommand().equals("+")
                 || e.getActionCommand().equals("-")
                 || e.getActionCommand().equals("*")
                 || e.getActionCommand().equals("/")
                 || e.getActionCommand().equals("^")) {
-
+            operator = e.getActionCommand();
         }
+        if (e.getActionCommand().equals("=") && operator != null) {
+            text = field.getText();
+            operands = text.split(operator);
+            operand1 = operands[0];
+            operand2 = operands[1];
+            result = Integer.parseInt(operand1) + Integer.parseInt(operand2);
+            displayToScreen(String.valueOf(result));
+        }
+    }
+
+    private void displayToScreen(String text) {
+        field.setText(text);
+        Font font = new Font("SansSerif", Font.BOLD, 50);
+        field.setFont(font);
     }
 }
