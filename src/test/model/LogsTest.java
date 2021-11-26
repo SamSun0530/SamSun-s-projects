e00e140684aebd94bccb3a4065153f1ac1c62176
AcorssHorizon <2298998611@qq.com>
2021-11-26 14:25:14 -0800

    Phase 4: Task 3

--- src/test/model/LogsTest.java
+++ src/test/model/LogsTest.java
@@ -28,4 +28,12 @@ public class LogsTest {
         assertEquals("2 minus 1 = 1", logs.get(1).getLog());
         assertEquals("logs:[1 plus 1 = 2, 2 minus 1 = 1]", logs.toString());
     }
+
+    @Test
+    public void testClearLogs() {
+        logs.add(new Log("1 plus 1 = 2"));
+        logs.add(new Log("2 minus 1 = 1"));
+        logs.clearLogs();
+        assertEquals(0, logs.size());
+    }
 }
